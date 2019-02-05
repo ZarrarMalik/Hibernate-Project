80fa83b051a381fa58990e7640b5dba6e11f01fc
zarrar <zarrarmalik81@gmail.com>
2019-02-05 18:37:55 +0000

    Tests and @Onetoone relationship between Student and Passport table

--- Jpa-hibernate/src/main/java/com/myself/jpa/hibernate/JpaHibernateApplication.java
+++ Jpa-hibernate/src/main/java/com/myself/jpa/hibernate/JpaHibernateApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.myself.jpa.hibernate.entity.Course;
 import com.myself.jpa.hibernate.repository.CourseRepository;
+import com.myself.jpa.hibernate.repository.StudentRepository;
 
 @SpringBootApplication
 public class JpaHibernateApplication implements CommandLineRunner{
@@ -19,14 +20,19 @@ private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private CourseRepository repository;
 
 
+	@Autowired
+	private CourseRepository courseRepository;
+
+	@Autowired
+	private StudentRepository studentRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHibernateApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		repository.playWithEntityManager();
-	}
-
+	public void run(String... arg0) throws Exception {
+		studentRepository.saveStudentWithPassport();
+		//repository.playWithEntityManager();
+	}	
 }
-
---
+++ Jpa-hibernate/src/test/java/com/myself/jpa/hibernate/repository/StudentRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.myself.jpa.hibernate.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myself.jpa.hibernate.JpaHibernateApplication;
+import com.myself.jpa.hibernate.entity.Course;
+import com.myself.jpa.hibernate.entity.Student;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=JpaHibernateApplication.class)
+public class StudentRepositoryTest {
+
+	
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	StudentRepository repository;
+	
+	@Autowired
+	EntityManager em;
+
+	//Session & Session Factory
+
+	
+	//EntityManager & Persistence Context
+	//Transaction
+	
+	@Test
+	public void someTest() {
+		repository.someOperationToUnderstandPersistenceContext();
+	}
+
+
+	@Test
+	@Transactional
+	public void retrieveStudentAndPassportDetails() {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("student -> {}", student);
+		logger.info("passport -> {}",student.getPassport());
+	}
+}
+
+
